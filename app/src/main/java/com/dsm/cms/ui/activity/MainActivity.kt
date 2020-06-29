8c9b9f81124c5c0ec44c422bc93c6254b8fdb38a
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-06-29 20:04:18 +0900

    CMS-11 [Add] Gradle Update 및 Package 변경

--- app/src/main/java/com/dsm/cms/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dsm/cms/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dsm.cms
+package com.dsm.cms.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dsm.cms.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/dsm/cms/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/dsm/cms/ui/activity/SplashActivity.kt
@@ -1,10 +1,10 @@
-package com.dsm.cms
+package com.dsm.cms.ui.activity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : Activity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(this, MainActivity::class.java)

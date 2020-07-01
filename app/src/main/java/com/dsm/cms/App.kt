264de5847cbdc001dc92a9e8f6bc56a711aa5729
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-07-01 10:58:45 +0900

    CMS-8 [Add] Add Basic Koin Modules

--- app/src/main/java/com/dsm/cms/App.kt
+++ app/src/main/java/com/dsm/cms/App.kt
@@ -1,5 +1,21 @@
 package com.dsm.cms
 
 import android.app.Application
+import com.dsm.cms.di.apiModule
+import com.dsm.cms.di.dataSourceModule
+import com.dsm.cms.di.repositoryModule
+import com.dsm.cms.di.viewModelModule
+import org.koin.core.context.startKoin
 
-class App: Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(dataSourceModule)
+            modules(repositoryModule)
+            modules(viewModelModule)
+            modules(apiModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/dsm/cms/di/ViewModelModule.kt
@@ -0,0 +1,7 @@
+package com.dsm.cms.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+}

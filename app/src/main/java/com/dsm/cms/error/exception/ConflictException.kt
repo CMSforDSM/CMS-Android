739a39c71b31e50e55effc5c661382820f34edc2
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-07-28 14:01:51 +0900

    CMS-31 [Add] Add ErrorHandler & Exception Files

---
+++ app/src/main/java/com/dsm/cms/error/exception/ConflictException.kt
@@ -0,0 +1,3 @@
+package com.dsm.cms.error.exception
+
+class ConflictException(e: Throwable) : RuntimeException(e)
---
+++ app/src/main/java/com/dsm/cms/error/exception/ForbiddenException.kt
@@ -0,0 +1,3 @@
+package com.dsm.cms.error.exception
+
+class ForbiddenException(e: Throwable) : RuntimeException(e)
